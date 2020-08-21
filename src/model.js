c0140e9465ec3a811be5882e8f5719a02ec79096
Sergey K <sergeykondr@gmail.com>
2020-08-21 22:51:10 +0300

    first commit

---
+++ src/model.js
@@ -0,0 +1,8 @@
+export const model = [
+    {type: 'title', value: 'Test Title', options: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A maiores minima perferendis quidem suscipit. Illum ipsa labore quos voluptates voluptatibus!', options: {}},
+    {type: 'textColumns', value: [
+      '1 text', '2 text', '3 text', '4 text'
+    ],  options: {}},
+    {type: 'title', value: '2 Title', options: {}},
+]
